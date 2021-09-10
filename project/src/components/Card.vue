3373cef7e3b8aab79dac4285b0e33227c662984c
Riccarpa <riccardopagni99@gmail.com>
2021-09-10 16:01:27 +0200

    dinamic country flags

--- project/src/components/Card.vue
+++ project/src/components/Card.vue
@@ -3,7 +3,14 @@
     <ul>
       <li>TITOLO: {{ moovie.title }}</li>
       <li>TITOLO ORIGINALE: {{ moovie.original_title }}</li>
-      <li>LINGUA: {{ moovie.original_language }}</li>
+      <li>
+        LINGUA:
+        <img
+          :src="flagRender()"
+          v-text="flagRender()"
+          :alt="moovie.original_language"
+        />
+      </li>
       <li>VOTO: {{ moovie.vote_average }}</li>
     </ul>
   </div>
@@ -13,11 +20,23 @@
 export default {
   name: "Card",
   props: ["moovie"],
+  methods: {
+    flagRender() {
+      if (this.moovie.original_language === "it") {
+        return require("../images/it.png");
+      } else if (this.moovie.original_language === "en") {
+        return require("../images/en.png");
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
   margin: 2rem;
+}
+img {
+  height: 30px;
 }
 </style>
