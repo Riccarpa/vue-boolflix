be0ca92e923a0df756881b508144efe90fca3f6d
Riccarpa <riccardopagni99@gmail.com>
2021-09-10 12:46:35 +0200

    component library and card created

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <main>
+      <Library />
+    </main>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Library from "@/components/Library.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Library,
   },
 };
 </script>
---
+++ project/src/components/Library.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="library">
+    <div class="container">
+      <Card />
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "Library",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
